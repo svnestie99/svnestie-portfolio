7561275f951940f42051445a08b1d34fb9800ea3
svnestie <danila.glushakov@mail.ru>
2023-04-15 19:46:24 +0300

    Update

--- src/components/projects/ProjectsContainer.jsx
+++ src/components/projects/ProjectsContainer.jsx
@@ -4,18 +4,11 @@ import twd from '../../assets/images/projects/LuAQQ59muck.jpg';
 import kbrd from '../../assets/images/projects/zZB9Fexzsr0.jpg';
 import vlrnt from '../../assets/images/projects/IpvS3_aNgUg.jpg';
 import wdr from '../../assets/images/projects/7rJk3PY9zUw.jpg';
+import todo from '../../assets/images/projects/c7kZdx-YM9s.jpg';
 import { BsGithub } from 'react-icons/bs';
 
 export const ProjectsContainerComponent = (props) => {
   const arrOfProjects = [
-    {
-      name: 'TWDcomics',
-      type: 'App',
-      image: twd,
-      link: '#',
-      tools: ['HTML', 'CSS', 'JavaScript'],
-      github: 'https://github.com/svnestie99/TWD-comics',
-    },
     {
       name: 'Virtual Keyboard',
       type: 'App',
@@ -24,6 +17,23 @@ export const ProjectsContainerComponent = (props) => {
       tools: ['HTML', 'CSS', 'JavaScript'],
       github: 'https://github.com/svnestie99/Virtual-Keyboard',
     },
+    {
+      name: 'TODO list',
+      type: 'App',
+      image: todo,
+      link: 'https://svnestie99.github.io/todolist/',
+      tools: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/svnestie99/todolist',
+    },
+    {
+      name: 'TWDcomics',
+      type: 'App',
+      image: twd,
+      link: '#',
+      tools: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/svnestie99/TWD-comics',
+    },
+
     {
       name: 'Valorant',
       type: 'Template',
